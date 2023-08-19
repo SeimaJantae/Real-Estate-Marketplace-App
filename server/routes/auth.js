34d8936bc9255d50b743994619bbb63657d8fa77
SeimaJantae <seimajantae@gmail.com>
2023-08-19 22:11:30 +0700

    Init project + create server and database + create user model

---
+++ server/routes/auth.js
@@ -0,0 +1,8 @@
+import express from "express";
+import * as auth from "../controllers/auth.js";
+
+const router = express.Router();
+
+router.get("/", auth.welcome);
+
+export default router;
---
+++ server/server.js
@@ -0,0 +1,30 @@
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+import mongoose from "mongoose";
+import { DATABASE } from "./config.js";
+import authRoutes from "./routes/auth.js";
+
+const app = express();
+
+// db
+mongoose
+  .connect(DATABASE)
+  .then(() => {
+    console.log("db connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+// middlewares
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(cors());
+
+// routes middleware
+app.use("/api", authRoutes);
+
+app.listen(8000, () => {
+  console.log("server running on port 8000");
+});
